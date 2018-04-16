d7fd7b68a8b7c74fbcc5e2962cc09780b6e59f42
Hanshaoshuai <361062939@qq.com>
2018-04-16 22:14:05 +0800

    add

--- fengzhuangHeZujian/app_scroll/index.js
+++ fengzhuangHeZujian/app_scroll/index.js
@@ -24,10 +24,10 @@ function uitan(pageNums,pushHtml,dom) {
                 endX = x;
                 endY = y;
                 var abs = Math.abs(y - startY)
-//              if (abs > 0 && abs < 888) {
-                    container.style.cssText = "will-change: transform; transform: translate(0px, " + (y - startY) + "px); -webkit-transform: translate(0px, " + (y - startY) + "px) translateZ(0px);";
-//              }
-                if((endY - startY) >100){//下拉刷新
+                if (abs > 0 && abs < 420) {
+                    container.style.cssText = "will-change: transform;transition:600ms cubic-bezier(.1, .57, .1, 1); -webkit-transition: 600ms cubic-bezier(.1, .57, .1, 1); transform: translate(0px, " + (y - startY) + "px); -webkit-transform: translate(0px, " + (y - startY) + "px) translateZ(0px);";
+                }
+                if((endY - startY) >220){//下拉刷新
                 	document.querySelector('.shuaXin').innerText='放手刷新'
                 }
             });
@@ -36,7 +36,7 @@ function uitan(pageNums,pushHtml,dom) {
                 if (Math.abs(endY - startY) > 0) {
                     container.style.cssText += "will-change: transform;transition:600ms cubic-bezier(.1, .57, .1, 1); -webkit-transition: 600ms cubic-bezier(.1, .57, .1, 1); transform: translate(0px,0px); -webkit-transform: translate(0px,0px) translateZ(0px);";
                 }
-                if((endY - startY) >100){//下拉刷新
+                if((endY - startY) >220){//下拉刷新
             		document.querySelector('.shuaXin').innerText='下拉刷新'
                 	if(length==1){
                 		datelist=[];
