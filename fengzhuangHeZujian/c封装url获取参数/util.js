d8db71450c0ce54a8dbeda08863a8fe5536aadb3
Hanshaoshuai <361062939@qq.com>
2018-04-18 21:45:32 +0800

    2018/4/18

--- fengzhuangHeZujian/c封装url获取参数/util.js
+++ fengzhuangHeZujian/c封装url获取参数/util.js
@@ -18,3 +18,17 @@ export function urlParse(){
 	}
 	return obj;
 }
+function getRequest() {  
+	var url = location.search; //获取url中"?"符后的字串  
+	var theRequest = new Object();  
+	if (url.indexOf("?") != -1) {  
+		var str = url.substr(1);  
+		strs = str.split("&");  
+		for (var i = 0; i < strs.length; i++) {  
+			theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);  
+		}  
+	}  
+	return theRequest;  
+}  
+ 
+console.log(getRequest());
