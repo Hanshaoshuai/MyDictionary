060b21f1ca4198f345dd68d86bac50ae9951b7e7
Hanshaoshuai <361062939@qq.com>
2018-04-21 14:55:55 +0800

    change

--- fengzhuangHeZujian/app_scroll/index.js
+++ fengzhuangHeZujian/app_scroll/index.js
@@ -1,6 +1,6 @@
 
 
-function uitan(dataLength,pageNums,scrollTop,pushHtml,dom) {
+function uitan(dataLength,pageNums,pushHtml,dom) {
     var ht = {},
     	length=1;
     ht.init = function(d) {
@@ -25,9 +25,9 @@ function uitan(dataLength,pageNums,scrollTop,pushHtml,dom) {
                 endY = y;
                 var abs = Math.abs(y - startY)
                 if (abs > 0 && abs < 388) {
-                    container.style.cssText +=
-                    "transition:600ms cubic-bezier(.1, .57, .1, 1);"+
-                    "-webkit-transition: 600ms cubic-bezier(.1, .57, .1, 1);"+
+                    container.style.cssText =
+                    "transition:400ms cubic-bezier(.1, .57, .1, 1);"+
+                    "-webkit-transition: 400ms cubic-bezier(.1, .57, .1, 1);"+
                     "-webkit-transform: translate(0px, " + (y - startY) + "px) translateZ(0);"+
                     "transform: translate(0px, " + (y - startY) + "px) translateZ(0);"+
                     "-webkit-backface-visibility: hidden;"+
@@ -41,14 +41,14 @@ function uitan(dataLength,pageNums,scrollTop,pushHtml,dom) {
 				   	"-o-perspective: 1000;"+
 				   	"perspective: 1000;";
                 }
-                if((endY - startY) >150){//下拉刷新
+                if((endY - startY) >120){//下拉刷新
             		document.querySelector('.shuaXin').innerText='放手刷新'
                 }
             });
             container.addEventListener('touchend', function(e) {
 //              e.preventDefault();
                 if (Math.abs(endY - startY) > 0) {
-                    container.style.cssText +=
+                    container.style.cssText =
                     "transition:600ms cubic-bezier(.1, .57, .1, 1);"+
                     "-webkit-transition: 600ms cubic-bezier(.1, .57, .1, 1);"+
                     "-webkit-transform: translate(0px,0px) translateZ(0);"+
@@ -64,18 +64,18 @@ function uitan(dataLength,pageNums,scrollTop,pushHtml,dom) {
 				   	"-o-perspective: 1000;"+
 				   	"perspective: 1000;";
                 }
-                if((endY - startY) >150){//下拉刷新
+                if((endY - startY) >120 && scrollTop==0){//下拉刷新
             		document.querySelector('.shuaXin').innerText='下拉刷新';
 //          		document.querySelector('.jiaZai').innerText='加载中'
-                	if(length==1 && scrollTop==0){
+                	if(length==1){
                 		datelist=[];
                 		pageNum=1;
                 		document.querySelector('.list ul').innerHTML='';
                 		pushHtml();
+                		length+=1;
                 	}
-                	length+=1;
+                	length=1;
                 }
-                length=1;
             });
         }
         /**
